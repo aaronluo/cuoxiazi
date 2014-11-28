fe45ac934041122347e3297d7ee125286929ad9c
leonwang99 <leonwang99@aliyun.com>
2014-11-28 11:35:51 +0800

    add qrcode package and zxing lib

--- 02_code/android/eOrderMobile/gen/com/innovaee/eorder/R.java
+++ 02_code/android/eOrderMobile/gen/com/innovaee/eorder/R.java
@@ -11,7 +11,34 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int mm_tab_btn=0x7f070000;
+        public static final int bg_color=0x7f050000;
+        public static final int contents_text=0x7f050001;
+        public static final int encode_view=0x7f050002;
+        public static final int grgray=0x7f05001a;
+        public static final int header=0x7f050019;
+        public static final int help_button_view=0x7f050003;
+        public static final int help_view=0x7f050004;
+        public static final int mm_tab_btn=0x7f05001b;
+        public static final int possible_result_points=0x7f050005;
+        public static final int result_image_border=0x7f050006;
+        public static final int result_minor_text=0x7f050007;
+        public static final int result_points=0x7f050008;
+        public static final int result_text=0x7f050009;
+        public static final int result_view=0x7f05000a;
+        public static final int sbc_header_text=0x7f05000b;
+        public static final int sbc_header_view=0x7f05000c;
+        public static final int sbc_layout_view=0x7f05000e;
+        public static final int sbc_list_item=0x7f05000d;
+        public static final int sbc_page_number_text=0x7f05000f;
+        public static final int sbc_snippet_text=0x7f050010;
+        public static final int share_text=0x7f050011;
+        public static final int share_view=0x7f050012;
+        public static final int status_text=0x7f050014;
+        public static final int status_view=0x7f050013;
+        public static final int transparent=0x7f050015;
+        public static final int viewfinder_frame=0x7f050016;
+        public static final int viewfinder_laser=0x7f050017;
+        public static final int viewfinder_mask=0x7f050018;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -21,8 +48,8 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -40,32 +67,60 @@ public final class R {
         public static final int themestore_common_foreground_pressed=0x7f02000c;
     }
     public static final class id {
-        public static final int classify_description=0x7f080002;
-        public static final int classify_image=0x7f080000;
-        public static final int classify_name=0x7f080001;
-        public static final int goods_gridview=0x7f080004;
-        public static final int main_tab_group=0x7f080005;
-        public static final int tab_goodsshop=0x7f080006;
-        public static final int tab_settings=0x7f080008;
-        public static final int tab_userorder=0x7f080007;
-        public static final int title_bar=0x7f080003;
+        /**  Messages IDs 
+         */
+        public static final int auto_focus=0x7f070000;
+        public static final int btn_add_qrcode=0x7f07001b;
+        public static final int btn_cancel_scan=0x7f07000e;
+        public static final int btn_scan_barcode=0x7f070018;
+        public static final int classify_description=0x7f070011;
+        public static final int classify_image=0x7f07000f;
+        public static final int classify_name=0x7f070010;
+        public static final int decode=0x7f070001;
+        public static final int decode_failed=0x7f070002;
+        public static final int decode_succeeded=0x7f070003;
+        public static final int encode_failed=0x7f070004;
+        public static final int encode_succeeded=0x7f070005;
+        public static final int et_qr_string=0x7f07001a;
+        public static final int goods_gridview=0x7f070013;
+        public static final int iv_qr_image=0x7f07001c;
+        public static final int launch_product_query=0x7f070006;
+        public static final int main_tab_group=0x7f070014;
+        public static final int preview_view=0x7f07000c;
+        public static final int quit=0x7f070007;
+        public static final int restart_preview=0x7f070008;
+        public static final int return_scan_result=0x7f070009;
+        public static final int search_book_contents_failed=0x7f07000a;
+        public static final int search_book_contents_succeeded=0x7f07000b;
+        public static final int tab_goodsshop=0x7f070015;
+        public static final int tab_settings=0x7f070017;
+        public static final int tab_userorder=0x7f070016;
+        public static final int title_bar=0x7f070012;
+        public static final int tv_scan_result=0x7f070019;
+        public static final int viewfinder_view=0x7f07000d;
     }
     public static final class layout {
-        public static final int classify_griditem=0x7f030000;
-        public static final int foodsshop_activity=0x7f030001;
-        public static final int maintab_activity=0x7f030002;
-        public static final int settings_activity=0x7f030003;
-        public static final int userorder_activity=0x7f030004;
+        public static final int capture=0x7f030000;
+        public static final int classify_griditem=0x7f030001;
+        public static final int foodsshop_activity=0x7f030002;
+        public static final int maintab_activity=0x7f030003;
+        public static final int qrtest=0x7f030004;
+        public static final int settings_activity=0x7f030005;
+        public static final int userorder_activity=0x7f030006;
+    }
+    public static final class raw {
+        public static final int beep=0x7f040000;
+        public static final int realm_properties=0x7f040001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f080002;
+        public static final int app_name=0x7f080000;
+        public static final int hello_world=0x7f080001;
         /**  Tab 
          */
-        public static final int tab_name_goodsshop=0x7f050003;
-        public static final int tab_name_settings=0x7f050005;
-        public static final int tab_name_userorder=0x7f050004;
+        public static final int tab_name_goodsshop=0x7f080003;
+        public static final int tab_name_settings=0x7f080005;
+        public static final int tab_name_userorder=0x7f080004;
     }
     public static final class style {
         /** 
@@ -73,6 +128,6 @@ public final class R {
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
     
          */
-        public static final int MMTabButton=0x7f060000;
+        public static final int MMTabButton=0x7f090000;
     }
 }
---
+++ 02_code/android/eOrderMobile/src/com/innovaee/eorder/mobile/qrcode/QrCodeTestActivity.java
@@ -0,0 +1,79 @@
+package com.innovaee.eorder.mobile.qrcode;
+
+
+import com.google.zxing.WriterException;
+import com.innovaee.eorder.R;
+import com.innovaee.eorder.mobile.zxing.activity.CaptureActivity;
+import com.innovaee.eorder.mobile.zxing.encoding.EncodingHandler;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QrCodeTestActivity extends Activity {
+	private TextView resultTextView;
+	private EditText qrStrEditText;
+	private ImageView qrImgImageView;
+		
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qrtest);
+        				
+        resultTextView = (TextView) this.findViewById(R.id.tv_scan_result);
+        qrStrEditText = (EditText) this.findViewById(R.id.et_qr_string);
+        qrImgImageView = (ImageView) this.findViewById(R.id.iv_qr_image);
+        
+        Button scanBarCodeButton = (Button) this.findViewById(R.id.btn_scan_barcode);
+        scanBarCodeButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+					
+				Intent openCameraIntent = new Intent(QrCodeTestActivity.this,CaptureActivity.class);
+				startActivityForResult(openCameraIntent, 0);
+			}
+		});
+        
+        Button generateQRCodeButton = (Button) this.findViewById(R.id.btn_add_qrcode);
+        generateQRCodeButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				try {
+					String contentString = qrStrEditText.getText().toString();
+					if (!contentString.equals("")) {
+						
+						Bitmap qrCodeBitmap = EncodingHandler.createQRCode(contentString, 350);
+						qrImgImageView.setImageBitmap(qrCodeBitmap);
+					}else {
+						Toast.makeText(QrCodeTestActivity.this, "Text can not be empty", Toast.LENGTH_SHORT).show();
+					}
+					
+				} catch (WriterException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+    }
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+	
+		if (resultCode == RESULT_OK) {
+			Bundle bundle = data.getExtras();
+			String scanResult = bundle.getString("result");
+			resultTextView.setText(scanResult);
+		}
+	}
+}
