c0ab72c87f39be0134e93d7eed4f66d7baf03d83
aaronluo <kinglz2003@hotmail.com>
2014-12-30 17:42:35 +0800

    add final to Constants variables

--- 02_code/web/eorder/src/main/java/com/innovaee/eorder/module/utils/Constants.java
+++ 02_code/web/eorder/src/main/java/com/innovaee/eorder/module/utils/Constants.java
@@ -15,15 +15,15 @@ package com.innovaee.eorder.module.utils;
  */
 public class Constants {
 
-    public static String REGEX = ",";
+    public static final String REGEX = ",";
 
     /**
      * 默认角色：普通用户
      */
-    public static Integer DEFAULT_ROLE = 2;
+    public static  final Integer DEFAULT_ROLE = 2;
 
     /**
      * 默认等级：普通会员
      */
-    public static Integer DEFAULT_LEVEL = 4;
+    public static final Integer DEFAULT_LEVEL = 4;
 }
