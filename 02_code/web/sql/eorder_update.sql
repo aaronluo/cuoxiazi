ea2d1cc9fa29b33fcdc8f5e004b8f922a6558e74
CoderDream <coderdream@gmail.com>
2015-03-09 23:20:18 +0800

    update sql script

--- 02_code/web/sql/eorder_update.sql
+++ 02_code/web/sql/eorder_update.sql
@@ -1,2 +1,2 @@
-﻿ALTER TABLE `t_order` ADD `discount_price` float(64,2) DEFAULT NULL;
+ALTER TABLE `t_order` ADD `discount_price` float(64,2) DEFAULT NULL;
 UPDATE `t_order` SET `discount_price`=`total_price` WHERE `id` > 0;
