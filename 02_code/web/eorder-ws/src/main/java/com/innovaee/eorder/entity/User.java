b12ad302903c78c41bf300f40250dde7b688b33e
aaronluo <kinglz2003@hotmail.com>
2015-03-26 23:26:00 +0800

    correct order seq

--- 02_code/web/eorder-ws/src/main/java/com/innovaee/eorder/entity/User.java
+++ 02_code/web/eorder-ws/src/main/java/com/innovaee/eorder/entity/User.java
@@ -18,6 +18,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
 /**
@@ -133,6 +134,7 @@ public class User extends BaseEntity {
     }
 
     @OneToMany(targetEntity = Order.class, fetch = FetchType.EAGER, mappedBy = "member")
+    @OrderBy("id desc")
     public Set<Order> getOrders() {
         return orders;
     }
