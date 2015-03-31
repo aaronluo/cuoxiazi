da943f1a072215ced774b7942c2db29baef4b451
aaronluo <kinglz2003@hotmail.com>
2015-03-31 11:18:08 +0800

    add order service UT

---
+++ 02_code/web/eorder-ws/src/main/java/com/innovaee/eorder/exception/OrderNotFoundException.java
@@ -0,0 +1,34 @@
+/***********************************************
+ * Filename        : Order.java
+ * Copyright      : Copyright (c) 2014
+ * Company        : Innovaee
+ * Created        : 03/30/2014
+ ************************************************/
+
+package com.innovaee.eorder.exception;
+
+import com.innovaee.eorder.support.MessageUtil;
+
+/**
+ * @Title: BaseEntity
+ * @Description: 实体基类
+ * 
+ * @version V1.0
+ */
+public class OrderNotFoundException extends BaseException {
+    static {
+        exceptionKey = "order_not_found_exception";
+    }
+    
+    private static final long serialVersionUID = 1L;
+
+    private String queryString;
+    
+    public OrderNotFoundException(String queryString) {
+        this.queryString = queryString;
+    }
+    
+    public String getMessage() {
+        return MessageUtil.getMessage("exceptionKey", queryString);
+    }
+}
